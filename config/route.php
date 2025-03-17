0e83c75514ed767cfd53a3b60d2c8b9bb172d0e7
PC-20250205WITV\Administrator <4343434@qq.com>
2025-03-17 15:11:38 +0800

    开发

--- config/route.php
+++ config/route.php
@@ -17,12 +17,12 @@ use Webman\Route;
 Route::get('/', [app\controller\IndexController::class, 'index']);
 
 Route::group('/v1', function () {
-    Route::any('/test/index', [app\controller\v1\TestController::class, 'index']);
+    Route::get('/test/index', [app\controller\v1\TestController::class, 'index']);
 
-    Route::any('/index/baseInfo', [app\controller\v1\IndexController::class, 'baseInfo']);
-    Route::any('/index/banner', [app\controller\v1\IndexController::class, 'banner']);
-    Route::any('/index/notice', [app\controller\v1\IndexController::class, 'notice']);
-    Route::any('/index/noticeList', [app\controller\v1\IndexController::class, 'noticeList']);
+    Route::get('/index/baseInfo', [app\controller\v1\IndexController::class, 'baseInfo']);
+    Route::get('/index/banner', [app\controller\v1\IndexController::class, 'banner']);
+    Route::get('/index/notice', [app\controller\v1\IndexController::class, 'notice']);
+    Route::get('/index/noticeList', [app\controller\v1\IndexController::class, 'noticeList']);
 
     Route::add(['POST', 'OPTIONS'], '/auth/login', [app\controller\v1\AuthController::class, 'login']);
     Route::add(['POST', 'OPTIONS'], '/notify/recharge', [app\controller\v1\NotifyController::class, 'recharge']);
@@ -31,22 +31,22 @@ Route::group('/v1', function () {
 
 Route::group('/v1', function () {
 
-    Route::any('/user/info', [app\controller\v1\UserController::class, 'info']);
-    Route::any('/user/referralList', [app\controller\v1\UserController::class, 'referralList']);
-    Route::any('/user/teamList', [app\controller\v1\UserController::class, 'teamList']);
+    Route::get('/user/info', [app\controller\v1\UserController::class, 'info']);
+    Route::get('/user/referralList', [app\controller\v1\UserController::class, 'referralList']);
+    Route::get('/user/teamList', [app\controller\v1\UserController::class, 'teamList']);
 
     Route::add(['POST', 'OPTIONS'], '/assets/recharge', [app\controller\v1\AssetsController::class, 'recharge']);
-    Route::any('/assets/rechargeList', [app\controller\v1\AssetsController::class, 'rechargeList']);
+    Route::get('/assets/rechargeList', [app\controller\v1\AssetsController::class, 'rechargeList']);
     Route::add(['POST', 'OPTIONS'], '/assets/withdraw', [app\controller\v1\AssetsController::class, 'withdraw']);
-    Route::any('/assets/withdrawList', [app\controller\v1\AssetsController::class, 'withdrawList']);
+    Route::get('/assets/withdrawList', [app\controller\v1\AssetsController::class, 'withdrawList']);
     Route::add(['POST', 'OPTIONS'], '/assets/exchange', [app\controller\v1\AssetsController::class, 'exchange']);
-    Route::any('/assets/getRate', [app\controller\v1\AssetsController::class, 'getRate']);
-    Route::any('/assets/assetsList', [app\controller\v1\AssetsController::class, 'assetsList']);
+    Route::get('/assets/getRate', [app\controller\v1\AssetsController::class, 'getRate']);
+    Route::get('/assets/assetsList', [app\controller\v1\AssetsController::class, 'assetsList']);
 
     Route::add(['POST', 'OPTIONS'], '/transaction/pledge', [app\controller\v1\TransactionController::class, 'pledge']);
     Route::add(['POST', 'OPTIONS'], '/transaction/mev', [app\controller\v1\TransactionController::class, 'mev']);
-    Route::any('/transaction/transactionList', [app\controller\v1\TransactionController::class, 'transactionList']);
-    Route::any('/transaction/transactionLogList', [app\controller\v1\TransactionController::class, 'transactionLogList']);
+    Route::get('/transaction/transactionList', [app\controller\v1\TransactionController::class, 'transactionList']);
+    Route::get('/transaction/transactionLogList', [app\controller\v1\TransactionController::class, 'transactionLogList']);
 
 })->middleware([
     app\middleware\JwtAuthMiddleware::class,
