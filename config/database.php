7482cf076762e2233f6fa77bed3c6e3d22ce0b02
DESKTOP-75M57DT\75615 <756152823@qq.com>
2026-01-20 19:29:39 +0800

    test

--- config/database.php
+++ config/database.php
@@ -4,11 +4,11 @@ return  [
     'connections' => [
         'mysql' => [
             'driver'      => 'mysql',
-            'host'        => 'localhost',
-            'port'        => '3389',
+            'host'        => '192.168.2.24',
+            'port'        => '3306',
             'database'    => 'ckb',
             'username'    => 'root',
-            'password'    => 'root',
+            'password'    => 'HC0803hc/*-',
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_general_ci',
             'prefix'      => '',
